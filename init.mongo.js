ad28f5c4655ea4831df4db27da2fe066806a966a
Andrew Sillers <apsillers@gmail.com>
2014-03-24 21:13:37 +0100

    Add user data to mongo init

--- init.mongo.js
+++ init.mongo.js
@@ -10,6 +10,11 @@ db.games.save({
 	{ "team": 2, "gold": 40, "username": "goodbye" }
     ]
 });
+
+// password for each account is "world"
+db.users.save({ "username" : "hello", "hash" : "sha1$b622db74$1$24bfe8583b3256bcd69664badb022f6542d81b1c" });
+db.users.save({ "username" : "goodbye", "hash" : "sha1$f2717d97$1$b753e89521086d6e026af2c1bc6f3ad3dc932bb9" });
+
 db.units.save({ "x" : 0, "y" : 0, "type" : "scout", "hp" : 17, "xp" : 0, "gameId" : 1, "team" : 1, "isCommander": true });
 db.units.save({ "x" : 6, "y" : 2, "type" : "grunt", "hp" : 25, "xp" : 0, "gameId" : 1, "team" : 2, "isCommander": true });
 
