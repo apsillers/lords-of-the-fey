67256f03776bd37d8b168d80ba7cf08080aba3c6
Andrew Sillers <apsillers@gmail.com>
2014-03-25 09:17:02 +0100

    Add moveLeft to mongo init units

--- init.mongo.js
+++ init.mongo.js
@@ -16,6 +16,5 @@ db.games.save({
 db.users.save({ "username" : "hello", "hash" : "sha1$b622db74$1$24bfe8583b3256bcd69664badb022f6542d81b1c" });
 db.users.save({ "username" : "goodbye", "hash" : "sha1$f2717d97$1$b753e89521086d6e026af2c1bc6f3ad3dc932bb9" });
 
-db.units.save({ "x" : 0, "y" : 0, "type" : "scout", "hp" : 17, "xp" : 0, "gameId" : 1, "team" : 1, "isCommander": true });
-db.units.save({ "x" : 6, "y" : 2, "type" : "grunt", "hp" : 25, "xp" : 0, "gameId" : 1, "team" : 2, "isCommander": true });
-
+db.units.save({ "x" : 0, "y" : 0, "type" : "scout", "hp" : 17, "xp" : 0, "gameId" : 1, "team" : 1, "moveLeft": 6, "isCommander": true });
+db.units.save({ "x" : 6, "y" : 2, "type" : "grunt", "hp" : 25, "xp" : 0, "gameId" : 1, "team" : 2, "moveLeft": 4, "isCommander": true });
