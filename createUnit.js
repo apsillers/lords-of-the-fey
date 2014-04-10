ed3109cb70cbb0aa9334c198aff7bfeca692cd95
Andrew Sillers <apsillers@gmail.com>
2014-04-10 11:06:54 +0200

    Enforce race-specific recruit list on the server-side

--- createUnit.js
+++ createUnit.js
@@ -3,41 +3,45 @@ var castlePathExists = require("./static/shared/castlePathExists");
 module.exports = function(data, mapData, collections, player, callback) {
     var gameId = data.gameId;
     var loadUnitType = require("./loadUtils").loadUnitType;
+    var loadRace = require("./loadUtils").loadRace;
+    
+    loadRace(player.race, function(err, race) {
+	if(race.recruitList.indexOf(data.type) == -1) { callback({}); }
 
-    collections.units.findOne({ gameId: gameId, x: data.x, y: data.y }, function(err, occupant) {
-	// if the space is populated, or it is not the user's turn, abort
-	if(occupant) {
-	    callback({});
-	}
-	
-	collections.units.find({ gameId: gameId, team: player.team, isCommander: true }, function(err, commanderCursor) {
-	    commanderCursor.toArray(function(err, commanders) {
-		var createValid = false;
-		
-		for(var i=0; i < commanders.length; ++i) {
-		    var commander = commanders[i];
+	collections.units.findOne({ gameId: gameId, x: data.x, y: data.y }, function(err, occupant) {
+	    // if the space is populated, abort
+	    if(occupant) {
+		callback({});
+	    }
+	    
+	    collections.units.find({ gameId: gameId, team: player.team, isCommander: true }, function(err, commanderCursor) {
+		commanderCursor.toArray(function(err, commanders) {
+		    var createValid = false;
 		    
-		    if(mapData[commander.x+","+commander.y].terrain.name == "keep" && // check that the commander is on a keep
-		       ["keep","castle"].indexOf(mapData[data.x+","+data.y].terrain.name) != -1 && // check target is a castle
-		       castlePathExists(commander, data, mapData) // find a castle-only path from commander to target
-		      ) { createValid = true; }
-		}
-		
-		if(!createValid) { callback({}); return; }
-		
-		loadUnitType(data.type, function(err, type) {
-		    data["xp"] = 0;
-		    data["hp"] = type.maxHp;
-		    data["moveLeft"] = type.move;
-		    data["team"] = player.team;
-		    collections.units.insert(data, function(err) {
-			if(!err) {
-			    callback(data);
-			}
+		    for(var i=0; i < commanders.length; ++i) {
+			var commander = commanders[i];
+			
+			if(mapData[commander.x+","+commander.y].terrain.name == "keep" && // check that the commander is on a keep
+			   ["keep","castle"].indexOf(mapData[data.x+","+data.y].terrain.name) != -1 && // check target is a castle
+			   castlePathExists(commander, data, mapData) // find a castle-only path from commander to target
+			  ) { createValid = true; }
+		    }
+		    
+		    if(!createValid) { callback({}); return; }
+		    
+		    loadUnitType(data.type, function(err, type) {
+			data["xp"] = 0;
+			data["hp"] = type.maxHp;
+			data["moveLeft"] = type.move;
+			data["team"] = player.team;
+			collections.units.insert(data, function(err) {
+			    if(!err) {
+				callback(data);
+			    }
+			});
 		    });
 		});
 	    });
 	});
     });
-}
-
+};
--- loadUtils.js
+++ loadUtils.js
@@ -4,13 +4,19 @@ exports.loadMap = function(filename, callback) {
     fs.readFile('static/data/maps/'+filename, { encoding: "utf8"}, function(err, data) {
         callback(err, toMapDict(data));
     });
-}
+};
 
 exports.loadUnitType = function(type, callback) {
     fs.readFile('static/data/units/'+type+".json", { encoding: "utf8"}, function(err, data) {
         callback(err, JSON.parse(data));
     });
-}
+};
+
+exports.loadRace = function(race, callback) {
+    fs.readFile('static/data/races/'+race+".json", { encoding: "utf8"}, function(err, data) {
+        callback(err, JSON.parse(data));
+    });
+};
 
 function toMapDict(map_data) {
     var misc_lines = 0;
