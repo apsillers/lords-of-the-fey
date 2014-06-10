162691597c69872369727d95d77a4a89f7838b23
Andrew Sillers <apsillers@gmail.com>
2014-06-10 21:12:05 +0300

    Minimap refactor

--- static/client/minimap.js
+++ static/client/minimap.js
@@ -5,8 +5,8 @@ var minimap = {
 
 	world.minimap = new createjs.Stage("minimap");
 
-	var spaceWidth = world.minimap.canvas.width / (world.maxX + 1);
-	var spaceHeight = world.minimap.canvas.height / (world.maxY + 1.5);
+	this.spaceWidth = world.minimap.canvas.width / (world.maxX + 1);
+	this.spaceHeight = world.minimap.canvas.height / (world.maxY + 1.5);
 
 	for(var i in mapDict) {
 	    var space = world.getSpaceByCoords(i);
@@ -14,11 +14,28 @@ var minimap = {
 	    var color = space.terrain.color;
 
 	    var minispace = new createjs.Shape();
-	    minispace.x = spaceWidth * space.x;
-	    minispace.y = spaceHeight * space.y + (space.x%2?0:spaceHeight/2);;
-	    minispace.graphics.beginFill(color).drawRect(0, 0, spaceWidth, spaceHeight);
+	    var minicoords = this.gridCoordsToMiniPixels(space);
+
+	    minispace.x = minicoords.x;
+	    minispace.y = minicoords.y;
+	    minispace.graphics.beginFill(color).drawRect(0, 0, this.spaceWidth, this.spaceHeight);
 	    world.minimap.addChild(minispace);
         }
 	world.minimap.update();
+    },
+    gridCoordsToMiniPixels: function(x,y) {
+	console.log("x" in x, x.y);
+	if("x" in x) {
+	    var inputCoords = x;
+	    var x = inputCoords.x;
+	    var y = inputCoords.y;
+	}
+	else if(typeof x == "string") { var coords = x.split(","); x = +coords[0]; y = +coords[1]; }
+
+	var output = {};
+	output.x = this.spaceWidth * x;
+	output.y = this.spaceHeight * y + (x%2?0:this.spaceHeight/2);
+
+	return output;
     }
 };
