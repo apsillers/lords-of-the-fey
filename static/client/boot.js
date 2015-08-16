cd1578a25db953bb3a14797a658495dfdb0fa6ad
Andrew Sillers <apsillers@gmail.com>
2015-08-15 20:45:30 -0400

    Add app cache update message

--- static/client/boot.js
+++ static/client/boot.js
@@ -256,3 +256,19 @@ var actionQueue = {
 	if(!ui.moveAnimating) { (this.queue.shift()||function(){})(); }
     }
 }
+
+// Check if a new cache is available on page load.
+window.addEventListener('load', function(e) {
+
+  window.applicationCache.addEventListener('updateready', function(e) {
+    if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+      // Browser downloaded a new app cache.
+      if (confirm('A new version of the client is available on page refresh. Reload the page?')) {
+        window.location.reload();
+      }
+    } else {
+      // Manifest didn't changed. Nothing new to server.
+    }
+  }, false);
+
+}, false);
