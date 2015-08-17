b5364262900b4af73fa97b190d63eb1bb2fa3414
Andrew Sillers <apsillers@gmail.com>
2015-08-17 07:09:58 -0400

    Implement fullscreen and animation-speed menu options

--- static/client/menu.js
+++ static/client/menu.js
@@ -7,6 +7,21 @@ var menuControl = {
             if(m.showingMenu) { m.hideMenu(); }
             else { m.showMenu(); }
         });
+
+        $("#top-menu-animation-speed-1").click(function() { ui.animationFactor = 0.66; m.hideMenu(); });
+        $("#top-menu-animation-speed-2").click(function() { ui.animationFactor = 0.44; m.hideMenu(); });
+        $("#top-menu-animation-speed-3").click(function() { ui.animationFactor = 0.33; m.hideMenu(); });
+        $("#top-menu-animation-speed-4").click(function() { ui.animationFactor = 0.22; m.hideMenu(); });
+        $("#top-menu-fullscreen").click(function() {
+            var body = document.body;
+            if(document.fullscreenElement || document.mozFullScreenElement ||
+               document.webkitFullscreenElement || document.msFullscreenElement) {
+                (document.exitFullscreen||document.webkitExitFullscreen||document.body.mozCancelFullScreen||elem.msExitFullscreen).call(document);
+            } else {
+                (body.requestFullscreen||body.webkitRequestFullscreen||document.body.mozRequestFullScreen||elem.msRequestFullscreen).call(body);
+            }
+            m.hideMenu();
+         });
     },
 
     showMenu: function() {
